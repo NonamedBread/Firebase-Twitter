1171144977e98eea5c854cd46e595af7a3184405
NonamedBread <lgh778923@gmail.com>
2023-05-17 23:02:32 +0900

    add : env

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByIkoIyhocF8vxqPCCDXmG3bPY0G7loIA",
-  authDomain: "fir-twittter.firebaseapp.com",
-  projectId: "fir-twittter",
-  storageBucket: "fir-twittter.appspot.com",
-  messagingSenderId: "179153268145",
-  appId: "1:179153268145:web:25035db2e6ce308635e190",
-  measurementId: "G-7DLTVNVEQL",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
 export const firebase = initializeApp(firebaseConfig);
