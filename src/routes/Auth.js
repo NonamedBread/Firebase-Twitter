72e5ae6d9357abbbfeebfaf8ec5ab6f5887c46e2
NonamedBread <lgh778923@gmail.com>
2023-05-22 14:12:42 +0900

    add : auth email&password box

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  console.log(email);
+  console.log(password);
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          defaultValue={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          defaultValue={password}
+          onChange={onChange}
+          required
+        />
+        <input type="submit" placeholder="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
