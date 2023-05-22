ae5aa9164c3c15384ba9f67fb5cd23153f3c03cc
NonamedBread <lgh778923@gmail.com>
2023-05-22 11:41:14 +0900

    fix : fabase.js -> fbase.js

--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -10,4 +11,4 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASURMENT_ID,
 };
 
-export const firebase = initializeApp(firebaseConfig);
+export default firebase.initializeApp(firebaseConfig);
