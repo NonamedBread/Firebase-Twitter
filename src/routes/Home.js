0672583953c42112af7569d4f1c16add620fdf5e
NonamedBread <lgh778923@gmail.com>
2023-05-23 21:31:46 +0900

    add: home input box

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [tweet, setTweet] = useState("");
+  const onSubmit = (e) => {
+    e.prventDeafault();
+  };
+  const onChange = (e) => {
+    const { value } = e.target;
+    setTweet(value);
+  };
+  console.log(tweet);
+  return (
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+          onChange={onChange}
+        ></input>
+      </form>
+    </div>
+  );
+};
 export default Home;
